005c975f2f8231daaa58af19edcb644edbd43399
Promise Ume <ngoziume1@gmail.com>
2021-10-08 17:20:47 +0100

    style

--- models/campground.js
+++ models/campground.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
-    Price:String,
+    image: String,
+    price: Number,
     description: String,
     location: String
 });
--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,11 @@ const seedDB = async () => {
       const random1000 = Math.floor(Math.random()*1000);
      const camp = new campground({
           location:`${cities[random1000].city},${cities[random1000].state}`,
-          title:`${sample(descriptors)} ${sample(places)}`
-
-      })
+          title:`${sample(descriptors)} ${sample(places)}`,
+          image:'https://unsplash.com/collections/483251',
+          description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus culpa omnis dignissimos tenetur facere ullam nemo, laborum optio doloribus distinctio velit eius architecto libero consequuntur saepe suscipit ad amet explicabo',
+          price: Math.floor(Math.random() * 20) + 10
+    })
       await camp.save();
   }
 }
